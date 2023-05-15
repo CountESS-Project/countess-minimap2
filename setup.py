4408f37ee9da81d09665c23789dcf01601c20058
Nick Moore <nick@zoic.org>
2023-05-15 10:21:42 +1000

    bump setup.py to 0.0.3 as well

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name = 'CountESS Plugin: Minimap2 Wrapper',
-        version = '0.0.2',
+        version = '0.0.3',
         author = 'Nick Moore',
         maintainer = 'Nick Moore',
         maintainer_email = 'nick@zoic.org',
