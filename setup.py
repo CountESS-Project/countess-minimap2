896c482fe8f2b65e308b20bb2126e926c9585b81
Nick Moore <nick@zoic.org>
2023-05-15 10:39:34 +1000

    fix package name :-(

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup
 
 setup(
-        name = 'CountESS Plugin: Minimap2 Wrapper',
+        name = 'countess-minimap2',
+        description = 'CountESS Plugin: Minimap2 Wrapper',
         version = '0.0.3',
         author = 'Nick Moore',
         maintainer = 'Nick Moore',
@@ -16,6 +17,11 @@ setup(
             'countess>=0.0.26,<0.1',
             'mappy~=2.24',
         ],
+        extras_require = {
+            "dev": [
+                "countess[dev]>=0.0.26,<0.1",
+            ]
+        },
         license_files = ('LICENSE.txt',),
         classifiers = [
             'Intended Audience :: Science/Research',
